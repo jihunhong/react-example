952d9d43825867f024b62e2facd1168eee06b392
jihunhong <redgee49@gmail.com>
2019-09-23 08:20:37 +0900

    Add gugudan example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React  from 'react';
+import {useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [first,   setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second,  setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value,   setValue] = useState('');
+  const [result,  setResult] = useState('');
+
+  const inputRef = useRef(null);
+
+  const onChangeInput = (e) => {
+      setValue(e.target.value);
+  }
+
+  const onSubmitForm = (e) => {
+      e.preventDefault();
+      if(parseInt(value) === first * second){
+          setResult(`${first} X ${second}는 ${value}`);
+          setFirst(Math.ceil(Math.random() * 9));
+          setSecond(Math.ceil(Math.random() * 9));
+          setValue('');
+          
+      }else{
+          setResult('땡');
+          setValue('');
+      }
+
+      inputRef.current.focus();
+  }
+
   return (
-    <div className="App">
-      React Example
-    </div>
-  );
+    <>
+        <div>{first} 곱하기 {second}는????</div>
+        <form onSubmit={onSubmitForm}>
+            <input ref={inputRef} type="number" value={value} onChange={onChangeInput}/>
+            <button>입력</button>
+        </form>
+        <div>{result}</div>
+    </>
+    );
 }
+
+
 
 export default App;
